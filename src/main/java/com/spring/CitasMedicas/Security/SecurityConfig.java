b8ebf7783120084d2557c03fde96f3ab8a1cbe69
kevincris119 <31830343+kevincris119@users.noreply.github.com>
2021-09-30 18:26:31 -0500

    Commit Regreso V1

--- src/main/java/com/spring/CitasMedicas/Security/SecurityConfig.java
+++ src/main/java/com/spring/CitasMedicas/Security/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 	        http
 	                .authorizeRequests()
-	                .antMatchers("/api/roles").hasRole("ADMINISTRADOR")                
+	                .antMatchers("/api/provincia").hasRole("ADMINISTRADOR")                
 	                .and()
 	                .formLogin();
 	}
