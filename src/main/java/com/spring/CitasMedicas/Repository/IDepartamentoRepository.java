3139d56888adeda2dcd80a2e375bd944317bb0a7
kevincris119 <31830343+kevincris119@users.noreply.github.com>
2021-10-22 19:51:30 -0500

    cambios Test

--- src/main/java/com/spring/CitasMedicas/Repository/IDepartamentoRepository.java
+++ src/main/java/com/spring/CitasMedicas/Repository/IDepartamentoRepository.java
@@ -1,7 +1,5 @@
 package com.spring.CitasMedicas.Repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.spring.CitasMedicas.Entity.Departamento;
